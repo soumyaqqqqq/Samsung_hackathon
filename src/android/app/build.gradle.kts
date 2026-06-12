770540f0d57cff66af5bd4c0a2f03a259cf96c6b
nirvik34 <jarhnirvik@gmail.com>
2026-06-12 23:51:50 +0530

    chore: fix trailing whitespace in versionCode property

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.friday.node"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 1 
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
